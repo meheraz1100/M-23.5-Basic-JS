b00b7a789f1a5b568cd1203b66dcbe3650ba4d2b
meheraz1100 <meheraz1100@gmail.com>
2024-02-19 15:48:13 +0600

    JS array

---
+++ array.js
@@ -0,0 +1,17 @@
+// const products = [
+//     {name : 'phone', price : 12000},
+//     {name : 'laptop', price : 32000},
+// ]
+
+// products[0]
+// products[1]
+
+// const products = {1,2,3,4,5]
+const products = {
+    '0' : 15,
+    '1' : 20,
+    '2' : 25,
+    '3' : 30,
+    '4' : 35,
+    '5' : 40,
+}
